8fad7871afdc4b1d2aad7ec467487cbf4937cfc7
shan18 <thegeek.004@gmail.com>
2019-01-14 10:43:31 +0000

    add separate capsnet model

---
+++ caps/utils.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+
+
+def load_image(path, size=None, color=False):
+    """
+    Load the image from the given file-path and resize it
+    to the given size if not None.
+    """
+
+    # Load the image using opencv
+    if color:  # BGR format
+        image = cv2.imread(path)
+    else:  # grayscale format
+        image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+    # Resize image if desired.
+    if not size is None:
+        image = cv2.resize(image, size)
+
+    # Convert image to numpy array and scale pixels so they fall between 0.0 and 1.0
+    image = np.array(image) / 255.0
+
+    # Convert 2-dim gray-scale array to 3-dim BGR array.
+    if (len(image.shape) == 2):
+        image = np.repeat(image[:, :, np.newaxis], 3, axis=2)
+
+    return image
+
+
+def print_progress_bar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """ Call in a loop to create terminal progress bar
+    :params iteration: current iteration (Int)
+    :params total: total iterations (Int)
+    :params prefix: prefix string (Str)
+    :params suffix: suffix string (Str)
+    :params decimals: positive number of decimals in percent complete (Int)
+    :params length: character length of bar (Int)
+    :params fill: bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
